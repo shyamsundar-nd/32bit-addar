83c8489ac355e490fc5eecd48be449bcd9283904
shyamsundar-nd <144580439+shyamsundar-nd@users.noreply.github.com>
2023-09-17 11:51:31 +0530

    Add files via upload

---
+++ addertb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.09.2023 02:47:36
+// Design Name: 
+// Module Name: addertb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module addertb();
+
+endmodule
